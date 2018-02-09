ad7ad64b1fbd2a6b60019b69d6d42c34eb1decac
Antje Kazimiers <antje@allesjetzt.net>
2018-02-09 10:21:33 -0500

    WIP

--- lightsheetInterface/app/settings.py
+++ lightsheetInterface/app/settings.py
@@ -2,5 +2,5 @@ class Settings:
    defaultFileBase = '/groups/lightsheet/lightsheet/home/ackermand/Lightsheet-Processing-Pipeline/Compiled_Functions/sampleInput_'
    outputDirectoryBase = '/groups/lightsheet/lightsheet/home/ackermand/interface_output/'
    lightsheetProcessing = 'http://10.36.13.18:9000/api/rest-v2/async-services/lightsheetProcessing'
-   lightSheetDB = 'mongodb://10.40.3.155:27017/'
+   lightSheetDB = 'mongodb://dev-mongodb:27017/lightsheet'
    username = 'lightsheet'
--- lightsheetInterface/run.py
+++ lightsheetInterface/run.py
@@ -1,3 +1,3 @@
 from app import app
 
-app.run(debug=True, host='10.36.13.18')
+app.run(debug=True, host='0.0.0.0', port=8089)
