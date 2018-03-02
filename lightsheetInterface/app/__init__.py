7acee918b755371d10a3801d7612fa980475f453
Antje Kazimiers <antje@allesjetzt.net>
2018-03-02 14:25:39 -0500

    Add template filter to format dates

--- lightsheetInterface/app/__init__.py
+++ lightsheetInterface/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_mongoengine import MongoEngine
 from flask_admin import Admin
+import dateutil
 
 app = Flask(__name__) #app variable, an object of class FLask
 app.secret_key = 'this_is_secret_key'
@@ -8,3 +9,12 @@ db = MongoEngine(app)
 admin=Admin(app)
 
 from app import views, models #app package from which views will be imported
+
+
+@app.template_filter('strftime')
+def _jinja2_filter_datetime(date, fmt=None):
+    date = dateutil.parser.parse(date)
+    native = date.replace(tzinfo=None)
+
+    format= "%Y-%m-%d at %H:%M:%S"
+    return native.strftime(format)
