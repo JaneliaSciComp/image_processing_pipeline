822c3ea1fa1a36c66d3166791a4161e0518ebb1a
Antje Kazimiers <antje@allesjetzt.net>
2018-03-16 11:47:03 -0400

    Fix form

--- lightsheetInterface/app/forms.py
+++ lightsheetInterface/app/forms.py
@@ -6,9 +6,5 @@ class StepForm(FlaskForm):
   name = StringField('Name', [validators.Length(min=6, max=35)])
 
   def __init__(self, record):
-          self.record = record
-
-  def compose():
-    # add dynamic fields
-    for key, value in self.record.items():
+    for key, value in record.items():
       setattr(self, key, StringField(value))
