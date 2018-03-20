340e744bd0116aec5f3675c299810c658c85d7a7
Antje Kazimiers <antje@allesjetzt.net>
2018-03-20 15:07:32 -0400

    Change settings, add new modules

--- lightsheetInterface/app/__init__.py
+++ lightsheetInterface/app/__init__.py
@@ -1,15 +1,14 @@
+import dateutil, ipdb
 from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
 from flask_cache import Cache
 from flask_mongoengine import MongoEngine
 from mongoengine import connect
 from flask_admin import Admin
-import dateutil
 
 app = Flask(__name__) #app variable, an object of class FLask
-app.secret_key = 'this_is_secret_key'
 
-# config settings
-app.config['CACHE_TYPE'] = 'simple'
+app.config.from_pyfile('lightsheet-config.cfg')
 app.cache = Cache(app)
 
 # db settings
@@ -19,11 +18,9 @@ app.config['MONGODB_SETTINGS'] = {
     'host': '10.40.3.155',
     'port': 27017
 }
+
 db = MongoEngine(app)
-
-# dev settings
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-
+toolbar = DebugToolbarExtension(app)
 from app import views, models #app package from which views will be imported
 
 @app.template_filter('strftime')
