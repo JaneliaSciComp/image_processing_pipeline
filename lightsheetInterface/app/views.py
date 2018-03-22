ea7b8fe25c32acbc6f26253ee6be65d6e9bac06a
Antje Kazimiers <antje@allesjetzt.net>
2018-03-22 15:26:51 -0400

    Step expansion works

--- lightsheetInterface/app/views.py
+++ lightsheetInterface/app/views.py
@@ -67,105 +67,106 @@ def index():
   pipelineSteps = {}
   formData = None
   countJobs = 0
+  stepData =  getJobStepData(job_id, client)
   # go through all steps and find those, which are used by the current job
   for step in config['steps']:
     currentStep = step.name
-    configData = getConfigurationsFromDB2(job_id, client, currentStep)
-    print(configData)
-    if configData != None and job_id != None:
+    if stepData != None and job_id != None:
       # If loading previous run parameters for specific step, then it should be checked and editable
-      editState = 'enabled'
-      checkboxState = 'checked'
-      countJobs += 1
-      forms = parseJsonData(configData)
-      # Pipeline steps is passed to index.html for formatting the html based
-      pipelineSteps[currentStep] = {
-        'stepName': step.name,
-        'stepDescription': step.description,
-        'inputJson': None,
-        'state': editState,
-        'checkboxState': checkboxState,
-        'forms': forms
-      }
+      for jobStep in stepData:
+        if currentStep == jobStep['name']:
+          editState = 'enabled'
+          checkboxState = 'checked'
+          countJobs += 1
+          forms = parseJsonData(stepData)
+          # Pipeline steps is passed to index.html for formatting the html based
+          pipelineSteps[currentStep] = {
+            'stepName': step.name,
+            'stepDescription': step.description,
+            'inputJson': None,
+            'state': editState,
+            'checkboxState': checkboxState,
+            'forms': forms
+          }
 
-    if request.method == 'POST':
-        #If a job is submitted (POST request) then we have to save parameters to json files and to a database and submit the job
-        #lightsheetDB is the database containing lightsheet job information and parameters
-        allSelectedStepNames=""
-        allSelectedTimePoints=""
-        stepParameters=[]
-        currentLightsheetCommit = subprocess.check_output(['git', '--git-dir', settings.pipelineGit, 'rev-parse', 'HEAD']).strip().decode("utf-8")
-        userDefinedJobName=[]
+  if request.method == 'POST':
+      #If a job is submitted (POST request) then we have to save parameters to json files and to a database and submit the job
+      #lightsheetDB is the database containing lightsheet job information and parameters
+      allSelectedStepNames=""
+      allSelectedTimePoints=""
+      stepParameters=[]
+      currentLightsheetCommit = subprocess.check_output(['git', '--git-dir', settings.pipelineGit, 'rev-parse', 'HEAD']).strip().decode("utf-8")
+      userDefinedJobName=[]
 
-        postedData = request.get_json()
-        if not postedData:
-            #Then submission from webpage itself
-            pipelineOrder = ['clusterPT', 'clusterMF', 'localAP', 'clusterTF', 'localEC', 'clusterCS', 'clusterFR']
-            for currentStep in pipelineOrder:
-                text = request.form.get(currentStep) #will be none if checkbox is not checked
-                if text is not None:
-                    #Store step parameters and step names/times to use as arguments for the post
-                    jsonifiedText = json.loads(text, object_pairs_hook=OrderedDict)
-                    stepParameters.append({"name":currentStep, "state":"NOT YET QUEUED", 
-                                           "creationTime":"N/A", "endTime":"N/A", 
-                                           "elapsedTime":"N/A","logAndErrorPath": "N/A",
-                                           "parameters": jsonifiedText})
-                    allSelectedStepNames = allSelectedStepNames+currentStep+","
-                    numTimePoints = math.ceil(1+(jsonifiedText["timepoints"]["end"] - jsonifiedText["timepoints"]["start"])/jsonifiedText["timepoints"]["every"])
-                    allSelectedTimePoints = allSelectedTimePoints+str(numTimePoints)+", "
-        
-            if stepParameters:
-                #Finish preparing the post body
-                allSelectedStepNames = allSelectedStepNames[0:-1]
-                allSelectedTimePoints = allSelectedTimePoints[0:-2]
-        else: #Then data posted
-            allSelectedStepNames=postedData["args"][1]
-            allSelectedTimePoints=postedData["args"][3]
-            stepParameters=postedData["args"][5]
+      postedData = request.get_json()
+      if not postedData:
+          #Then submission from webpage itself
+          pipelineOrder = ['clusterPT', 'clusterMF', 'localAP', 'clusterTF', 'localEC', 'clusterCS', 'clusterFR']
+          for currentStep in pipelineOrder:
+              text = request.form.get(currentStep) #will be none if checkbox is not checked
+              if text is not None:
+                  #Store step parameters and step names/times to use as arguments for the post
+                  jsonifiedText = json.loads(text, object_pairs_hook=OrderedDict)
+                  stepParameters.append({"name":currentStep, "state":"NOT YET QUEUED",
+                                         "creationTime":"N/A", "endTime":"N/A",
+                                         "elapsedTime":"N/A","logAndErrorPath": "N/A",
+                                         "parameters": jsonifiedText})
+                  allSelectedStepNames = allSelectedStepNames+currentStep+","
+                  numTimePoints = math.ceil(1+(jsonifiedText["timepoints"]["end"] - jsonifiedText["timepoints"]["start"])/jsonifiedText["timepoints"]["every"])
+                  allSelectedTimePoints = allSelectedTimePoints+str(numTimePoints)+", "
+
+          if stepParameters:
+              #Finish preparing the post body
+              allSelectedStepNames = allSelectedStepNames[0:-1]
+              allSelectedTimePoints = allSelectedTimePoints[0:-2]
+      else: #Then data posted
+          allSelectedStepNames=postedData["args"][1]
+          allSelectedTimePoints=postedData["args"][3]
+          stepParameters=postedData["args"][5]
+
+      if stepParameters: #make sure post is not empty
+          if not userDefinedJobName:
+              #userDefinedJobName = requestOutputJsonified["_id"]
+              userDefinedJobName=""
+
+          dataToPostToDB = {"jobName": userDefinedJobName,
+                            "state": "NOT YET QUEUED",
+                            "lightsheetCommit":currentLightsheetCommit,
+                            "selectedStepNames": allSelectedStepNames,
+                            "selectedTimePoints": allSelectedTimePoints,
+                            "steps": stepParameters
+                           }
+          newId = lightsheetDB.jobs.insert_one(dataToPostToDB).inserted_id
+          configAddress = settings.serverInfo['fullAddress'] + "config/" + str(newId)
+          postBody = { "processingLocation": "LSF_JAVA",
+                       "args": ["-configAddress",configAddress,
+                                "-allSelectedStepNames",allSelectedStepNames,
+                                "-allSelectedTimePoints",allSelectedTimePoints],
+                       "resources": {"gridAccountId": "lightsheet"}
+                   }
+          requestOutput = requests.post(settings.devOrProductionJACS + '/async-services/lightsheetProcessing',
+                                        headers=getHeaders(),
+                                        data=json.dumps(postBody))
+          requestOutputJsonified = requestOutput.json()
+          creationDate = newId.generation_time
+          creationDate = str(creationDate.replace(tzinfo=UTC).astimezone(eastern))
+          lightsheetDB.jobs.update_one({"_id":newId},{"$set": {"jacs_id":requestOutputJsonified["_id"], "configAddress":configAddress, "creationDate":creationDate[:-6]}})
             
-        if stepParameters: #make sure post is not empty
-            if not userDefinedJobName:
-                #userDefinedJobName = requestOutputJsonified["_id"]
-                userDefinedJobName=""
-        
-            dataToPostToDB = {"jobName": userDefinedJobName,
-                              "state": "NOT YET QUEUED",
-                              "lightsheetCommit":currentLightsheetCommit, 
-                              "selectedStepNames": allSelectedStepNames, 
-                              "selectedTimePoints": allSelectedTimePoints,
-                              "steps": stepParameters
-                             }
-            newId = lightsheetDB.jobs.insert_one(dataToPostToDB).inserted_id
-            configAddress = settings.serverInfo['fullAddress'] + "config/" + str(newId)
-            postBody = { "processingLocation": "LSF_JAVA",
-                         "args": ["-configAddress",configAddress,
-                                  "-allSelectedStepNames",allSelectedStepNames,
-                                  "-allSelectedTimePoints",allSelectedTimePoints],
-                         "resources": {"gridAccountId": "lightsheet"}
-                     }
-            requestOutput = requests.post(settings.devOrProductionJACS + '/async-services/lightsheetProcessing',
-                                          headers=getHeaders(),
-                                          data=json.dumps(postBody))
-            requestOutputJsonified = requestOutput.json()
-            creationDate = newId.generation_time
-            creationDate = str(creationDate.replace(tzinfo=UTC).astimezone(eastern))
-            lightsheetDB.jobs.update_one({"_id":newId},{"$set": {"jacs_id":requestOutputJsonified["_id"], "configAddress":configAddress, "creationDate":creationDate[:-6]}})
-            
-    #JACS service states
-    # if any are not Canceled, timeout, error, or successful then 
-    #updateLightsheetDatabaseStatus
-    updateDBStatesAndTimes(lightsheetDB)
-    parentJobInfo = getJobInfoFromDB(lightsheetDB, job_id,"parent")
-  
-    #Return index.html with pipelineSteps and serviceData
-    return render_template('index.html',
-                           title='Home',
-                           pipelineSteps=pipelineSteps,
-                           parentJobInfo = parentJobInfo,
-                           logged_in=True,
-                           config = config,
-                           version = getAppVersion(app.root_path),
-                           lightsheetDB_id = job_id)
+          #JACS service states
+          # if any are not Canceled, timeout, error, or successful then
+          #updateLightsheetDatabaseStatus
+          updateDBStatesAndTimes(lightsheetDB)
+
+  parentJobInfo = getJobInfoFromDB(lightsheetDB, job_id,"parent")
+  #Return index.html with pipelineSteps and serviceData
+  return render_template('index.html',
+                         title='Home',
+                         pipelineSteps=pipelineSteps,
+                         parentJobInfo = parentJobInfo,
+                         logged_in=True,
+                         config = config,
+                         version = getAppVersion(app.root_path),
+                         lightsheetDB_id = job_id)
 
 
 @app.route('/job_status', methods=['GET'])
@@ -194,12 +195,12 @@ def job_status():
 def search():
     return render_template('search.html',
                            logged_in=True,
-                           version = app_version)
+                           version = getAppVersion(app.root_path))
 
 @app.route('/config/<lightsheetDB_id>', methods=['GET'])
 def config(lightsheetDB_id):
     stepName = request.args.get('stepName')
-    output=getConfigurationsFromDB(lightsheetDB_id, stepName)
+    output=getConfigurationsFromDB(lightsheetDB_id, client, stepName)
     if output==404:
         abort(404)
     else:
