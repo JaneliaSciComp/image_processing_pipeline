ff7afb9dc21b6b30523c214c58f6604b5d3c6789
Antje Kazimiers <antje@allesjetzt.net>
2018-04-05 11:20:06 -0400

    Load job from datatable

--- lightsheetInterface/app/static/js/table.js
+++ lightsheetInterface/app/static/js/table.js
@@ -1,3 +1,9 @@
+var ls_table = ls_table || {};
+ls_table.loadRow = function(){
+  var job_id = $(this).find('.job-id').text();
+  window.location = '?lightsheetDB_id=' + job_id;
+},
+
 $(document).ready(() => {
   let table = null;
   if (table_data) {
@@ -23,6 +29,7 @@ $(document).ready(() => {
       },
       {
         title: 'Job ID (and Configuration Link)',
+        className: 'job-id',
         data: 'id',
         render(data, type, row, meta) {
           return data ? "<a href=\"/config/" + data +  "\">" + data + "</a>": '';
@@ -35,4 +42,5 @@ $(document).ready(() => {
       ],
     });
   }
+  $('#job-table tr').click(ls_table.loadRow);
 });
