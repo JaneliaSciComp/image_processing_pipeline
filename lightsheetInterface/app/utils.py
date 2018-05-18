9b77927d1bdb1cc7d234f800ca07e90a9d08e467
Antje Kazimiers <antje@allesjetzt.net>
2018-05-18 09:23:20 -0400

    Change matplotlib import

--- lightsheetInterface/app/utils.py
+++ lightsheetInterface/app/utils.py
@@ -1,5 +1,7 @@
 # Contains helper functons used in views.py for the most part
 import numpy, datetime, glob, scipy, re, json, requests, os, ipdb, re
+import matplotlib
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from wtforms import *
