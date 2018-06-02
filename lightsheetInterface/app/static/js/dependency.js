88166bb2987c0719e640a748390b213c7e0f3938
David Ackerman <ackermand@vm638.int.janelia.org>
2018-06-01 20:35:51 -0400

    Improved global parmeters and now provide link to download error directory

--- lightsheetInterface/app/static/js/dependency.js
+++ lightsheetInterface/app/static/js/dependency.js
@@ -7,15 +7,58 @@ var dependency = dependency || {};
 dependency.applyGlobalParameter = function(){
   if (value_dependencies) {
     for (var t = 0; t < value_dependencies.length; t++) {
-      if (value_dependencies.pattern) {
+      var globalId = Mustache.render("globalParameters-{{input}}", value_dependencies[t]);
+      var stepId = Mustache.render("{{step}}-{{output}}", value_dependencies[t]);
+      var globalElem = document.getElementById(globalId);
+      var stepElem = document.getElementById(stepId);
+      if (value_dependencies[t].pattern) {
         // Do something with the given pattern
+        var pattern=value_dependencies[t].pattern;
+        var variables = pattern.match(/[^\{\]]+(?=\})/g);
+        for (var i=0; i<variables.length; i++){
+          var currentVariableId;
+          currentVariableId = "globalParameters-"+variables[i];
+          needToFormat=false;
+          if (currentVariableId.includes("_string")){
+            needToFormat=true;
+            currentVariableId=currentVariableId.replace("_string","");
+          }
+          if (currentVariableId.includes("cameras") || currentVariableId.includes("channels")){
+            currentVariableValue = getCheckboxVal(currentVariableId);
+          }
+          else{
+            var globalElem = document.getElementById(currentVariableId);
+            var globalInputs = globalElem.getElementsByTagName('input');
+            var currentVariableValue = globalInputs[0].value;
+          }
+          if(needToFormat){
+            var prefix;
+            if (currentVariableId.includes("specimen")){
+              prefix="SPM";
+              iterations = 1; //need this because this is a string and don't want to iterate through each character
+            }
+            else if (currentVariableId.includes("cameras")){
+              prefix="CM";
+              iterations = currentVariableValue.length;
+            }
+            else if (currentVariableId.includes("channels")){
+              prefix="CHN";
+              iterations=currentVariableValue.length;
+            }
+            var currentVariableValueFormatted="";
+            for(var j=0; j<iterations;j++){
+              currentVariableValueFormatted = currentVariableValueFormatted + (prefix+"0"+currentVariableValue[j].replace(".0","")+"_");
+            }
+            currentVariableValue = currentVariableValueFormatted.slice(0,-1); //Remove trailing "_"
+          }
+          pattern = pattern.replace("{"+variables[i]+"}",currentVariableValue);
+        }
+        pattern = pattern.replace("//","/");
+        var stepInputs = stepElem.getElementsByTagName('input');
+        stepInputs[0].value = pattern;
       }
       else {
         // We're dealing with array or range parameters
-        var globalId = Mustache.render("globalParameters-{{input}}", value_dependencies[t]);
-        var stepId = Mustache.render("{{step}}-{{output}}", value_dependencies[t]);
-        var globalElem = document.getElementById(globalId);
-        var stepElem = document.getElementById(stepId);
         if (globalElem && stepElem) {
           var globalInputs = globalElem.getElementsByTagName('input');
           var stepInputs = stepElem.getElementsByTagName('input');
@@ -61,6 +104,16 @@ dependency.applyGlobalParameter = function(){
             sObj['end'].value = gObj['end'];
             sObj['every'].value = gObj['every'];
           }
+          else if (value_dependencies[t].formatting == 'C'){
+            //Then we are working with a selectpicker checkbox
+            var globalId = Mustache.render("select_{{input}}", value_dependencies[t]);
+            var globalCheckbox = $('select[id=' + globalId +']');
+            var stepId = Mustache.render("select_{{output}}", value_dependencies[t]);
+            var stepCheckbox = $('select[id=' + stepId +']');
+            //Set the value and update it
+            stepCheckbox.val(globalCheckbox.val());
+            stepCheckbox.change();
+          }
           else {
             stepInputs[0].value = globalInputs[0].value;
           }
@@ -81,4 +134,9 @@ dependency.addParameter = function(element){
   else {
     outputField.value = JSON.parse(result);
   }
+}
+
+getCheckboxVal = function(checkbox_id){
+  var stepCheckbox = $('select[id=select_' + checkbox_id.replace("globalParameters-","") +']');
+  return stepCheckbox.val();
 }
