434bbb9dedc46fc6a6a1615c9f9f7f7940378777
Antje Kazimiers <antje@allesjetzt.net>
2018-10-31 10:29:38 -0400

    0.12.1

--- lightsheetInterface/wsgi.py
+++ lightsheetInterface/wsgi.py
@@ -1,5 +1,6 @@
-from app import app
-
+import sys, os
+sys.path = sys.path + [os.path.dirname(__file__)]
+from app import app as application
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
