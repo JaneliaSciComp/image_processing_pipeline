8ed058a2b71aae9f71b3bee76ffc5cd0e2cc3fcc
David Ackerman <ackermand@janelia.hhmi.org>
2019-04-02 10:55:32 -0400

    Cleaned up reformat_data_to_post function

--- lightsheetInterface/app/jobs_io.py
+++ lightsheetInterface/app/jobs_io.py
@@ -5,7 +5,6 @@ from flask import abort
 from flask_login import current_user
 from mongoengine.queryset.visitor import Q
 from app.models import Step, Parameter
-from enum import Enum
 from app import app
 from app.utils import submit_to_jacs, get_job_step_data, get_parameters
 from bson.objectid import ObjectId
@@ -21,57 +20,30 @@ def reformat_data_to_post(posted_data, for_submission=True):
 
     if posted_data and posted_data != {}:
         for step in posted_data.keys():
+            parameters_to_empty = []
             # first part: get the parameter values into lists
-            step_parameter_result = {}
+            step_parameter_reformatted = {}
             sorted_parameters = sorted(posted_data[step][p].keys())
-            checkboxes = []
-
             for parameter_key in sorted_parameters:
+                parameter_name = parameter_key.rsplit('_', 1)[0]
+
                 # Find checkboxes and deal with them separately
-                if 'checkbox' in parameter_key:
-                    checkboxes.append(parameter_key)
+                if 'emptycheckbox_' in parameter_key and posted_data[step][p][parameter_key].upper() == 'TRUE':
+                    parameters_to_empty.append( parameter_name.split('_',1)[-1] )
 
-                is_range_parameter = False
-                # First test if it's a nested parameter
-                if '-' in parameter_key:  # check, whether this is a range parameter
-                    split_rest = parameter_key.split('-')
-                    q = Parameter.objects.filter(Q(formatting='R') & Q(name=parameter_key.split('-')[0] + '_' + step))
-                    if (len(q) != 0):  # this parameter is a range parameter
-                        is_range_parameter = True
-
-                # Then check if stepname is part of parameter name
-                if '_' in parameter_key:
-                    # TODO: check, if part after underscore is really a step name or _ part of parameter name
-                    split = parameter_key.rsplit('_', 1)
-                    parameter = split[0]
-                else:
-                    parameter = parameter_key
-
-                if is_range_parameter:
-                    param_value_set = step_parameter_result[parameter] if ( parameter in step_parameter_result ) else {}
+                if ('-' in parameter_key) and Parameter.objects.filter(Q(formatting='R') & Q(name=parameter_key.split('-')[0])):
+                    range_key = parameter_key.split('-')[1] #start, end or every
+                    param_value_set = step_parameter_reformatted[parameter_name] if parameter_name in step_parameter_reformatted else {}
                     # move the parts of the range parameter to the right key of the object
-                    if split_rest[1] in ['start', 'every', 'end']:
+                    if range_key in ['start', 'every', 'end']:
                         current_value = posted_data[step][p][parameter_key]
-                        if current_value == "empty":
-                            is_range_parameter = False
-                        else:
-                            param_value_set[split_rest[1]] = float(current_value) if current_value is not '' and current_value != "[]" else ''
-
-                    if is_range_parameter:
-                        # update the object
-                        step_parameter_result[parameter] = param_value_set
-
-                if not is_range_parameter:  # no range
-                    if parameter in step_parameter_result:
-                        param_value_set = step_parameter_result[parameter]
-                    else:
-                        param_value_set = []
-                    if not param_value_set:
-                        param_value_set = []
-                    step_parameter_result[parameter] = param_value_set
-
+                        param_value_set[range_key] = float(current_value) if current_value is not '' and current_value != "[]" else ''
+                        step_parameter_reformatted[parameter_name] = param_value_set
+                else:  # no range
+                    param_value_set = []
                     # check if current value is a float within a string and needs to be converted
                     current_value = posted_data[step][p][parameter_key]
+
                     if re.match("[-+]?[0-9]*\.?[0-9]*.$", current_value) is None:  # no float
                         try:
                             tmp = json.loads(current_value)
@@ -81,46 +53,39 @@ def reformat_data_to_post(posted_data, for_submission=True):
                     else:  # it's actual a float value -> get the value
                         param_value_set.append(float(current_value))
 
-            checkboxes_clean = []
-            for parameter in checkboxes:
-                if posted_data[step][p][parameter] == 'true':
-                    checkboxes_clean.append(parameter.split('-')[1].split('_')[0])
+                    step_parameter_reformatted[parameter_name] = param_value_set
 
-            if 'emptycheckbox' in step_parameter_result.keys():
-                step_parameter_result.pop('emptycheckbox')
 
             # cleanup step / second part: for lists with just one element, get the element
-            for parameter in step_parameter_result:
-                if parameter in checkboxes_clean:
-                    step_parameter_result[parameter] = []
+            for parameter_name in step_parameter_reformatted:
+                if parameter_name in parameters_to_empty:
+                    step_parameter_reformatted[parameter_name] = []
                 else:
-                    if type(step_parameter_result[parameter]) is list:
-                        if len(step_parameter_result[parameter]) == 1:
-                            step_parameter_result[parameter] = step_parameter_result[parameter][0]
+                    if type(step_parameter_reformatted[parameter_name]) is list:
+                        if len(step_parameter_reformatted[parameter_name]) == 1:
+                            step_parameter_reformatted[parameter_name] = step_parameter_reformatted[parameter_name][0]
                         else:
-                            for elem in step_parameter_result[parameter]:
-                                if elem == "" and len(set(step_parameter_result[parameter])) == 1:
-                                    step_parameter_result[parameter] = []
+                            for elem in step_parameter_reformatted[parameter_name]:
+                                if elem == "" and len(set(step_parameter_reformatted[parameter_name])) == 1:
+                                    step_parameter_reformatted[parameter_name] = []
                                     break
 
-            #replace with step_info function
+           # add some optional paramters
             step_result = {}
             step_result['name'] = step
-            # add some optional paramters
-            if 'type' in posted_data[step].keys():
-                step_result['type'] = posted_data[step]['type']
-            if 'bindPaths' in posted_data[step].keys():
-                step_result['bindPaths'] = posted_data[step]['bindPaths']
-            if 'pause' in posted_data[step].keys():
-                step_result['pause'] = posted_data[step]['pause']
+            for submission_parameter in ['type', 'bindPaths', 'pause']:
+                if submission_parameter in posted_data[step].keys():
+                    step_result[submission_parameter] = posted_data[step][submission_parameter]
+
             if for_submission:
                 step_result['state'] = 'NOT YET QUEUED'
-            step_result['parameters'] = step_parameter_result
+            step_result['parameters'] = step_parameter_reformatted
             temp_reformatted_data.append(OrderedDict(step_result))
 
             app.logger.info(temp_reformatted_data)
 
         all_steps = Step.objects.all().order_by('order')
+
         if all_steps:
             for step in all_steps:
                 current_step_dictionary = next((dictionary for dictionary in temp_reformatted_data if dictionary["name"] == step.name), None)
@@ -214,8 +179,7 @@ def load_preexisting_job(image_processing_db, image_processing_db_id, reparamete
     load_status = None
     pipeline_steps = OrderedDict()
     job_data = get_job_step_data(image_processing_db_id, image_processing_db)  # get the data for all jobs
-    preexisting_job_username = list(image_processing_db.jobs.find({"_id": ObjectId(image_processing_db_id)}, {"username": 1}))
-    username = preexisting_job_username[0]["username"]
+    username = list(image_processing_db.jobs.find({"_id": ObjectId(image_processing_db_id)}, {"username": 1}))[0]["username"]
     able_to_reparameterize = True
     succeded_but_latter_step_failed = []
 
